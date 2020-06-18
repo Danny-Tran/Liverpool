1ab4983a62a68a98420adb7b86319cdb02e8969a
Danny-Tran <truong_tran@live.com>
2020-06-18 16:01:31 -0600

    adding layout.js

---
+++ src/HighOrderComponent/Layout.js
@@ -0,0 +1,13 @@
+const { unstable_renderSubtreeIntoContainer } = require("react-dom");
+
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Layout;
