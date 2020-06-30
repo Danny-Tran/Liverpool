c38da41d8b777927a0aeecd78cb9b8bef858c76c
Danny-Tran <truong_tran@live.com>
2020-06-30 15:15:58 -0600

    complete stripes

--- src/HighOrderComponent/Layout.js
+++ src/HighOrderComponent/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../Component/Header_Footer/Header'
 import Footer from '../Component/Header_Footer/Footer'
-const { unstable_renderSubtreeIntoContainer } = require("react-dom");
+
 
 const Layout = (props) => {
     return (
