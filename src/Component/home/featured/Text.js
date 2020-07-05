1ef4e4f032a8bcd82964f6bc4904f3c94e6ba109
Danny-Tran <truong_tran@live.com>
2020-07-05 11:01:57 -0600

    adding featured player animate

--- src/Component/home/featured/Text.js
+++ src/Component/home/featured/Text.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
 
+import featuredPlayer from '../../../Resources/images/Feature_player-Mo.png'
 
 class Text extends Component {
 
@@ -67,13 +68,77 @@ class Text extends Component {
                 )
             }}
         </Animate>
+    );
+
+    animateSecond = () => (
+        <Animate
+            show={true}
+            start={{
+                opacity: 0,
+                x: 503,
+                y: 586
+            }}
+
+            enter={{
+                opacity: [1],
+                x: [273],
+                y: [586],
+                timing: {duration: 300, delay:500, ease: easePolyOut}
+            }}
+        >
+            {({opacity, x, y})=>{
+                return (
+                    <div 
+                        className='featured_second'
+                        style={{
+                            opacity,
+                            transform: `translate(${x}px, ${y}px)`,
+                            fontFamily:'Roboto'
+                        }}
+                    >
+                        Championships
+                    </div>
+                )
+            }}
+        </Animate>
+    );
+
+    animatePlayer = () => (
+        <Animate
+        show={true}
+        start={{
+            opacity: 0,
+        }}
+
+        enter={{
+            opacity: [1],
+            timing: {duration: 800, delay:500, ease: easePolyOut}
+        }}
+    >
+        {({opacity, x, y})=>{
+            return (
+                <div 
+                    className='featured_player'
+                    style={{
+                        opacity,
+                        background: `url(${featuredPlayer})` ,
+                        transform: `translate(550px, 201px)`,
+                    }}
+                >
+                    
+                </div>
+            )
+        }}
+    </Animate>
     )
 
     render() {
         return (
             <div className='featured_text'>
+                {this.animatePlayer()}
                 {this.animatedNumber()}
                 {this.animateFirst()}
+                {this.animateSecond()}
             </div>
         );
     }
