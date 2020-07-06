d280db4292293cc7b61dc49b53aac44b2f5a5294
Danny-Tran <truong_tran@live.com>
2020-07-06 12:30:31 -0600

    add tag blocks place holder

---
+++ src/Component/home/matches/Matches.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Matches = () => {
+    return (
+        <div className= 'home_matches_wrapper'>
+            <div className= 'container'>
+                TAG
+
+                BLOCK
+
+                TAG
+                
+            </div>
+        </div>
+    );
+};
+
+export default Matches;
