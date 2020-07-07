2721bea7ea5e0406ff80018867543a5dcfd75414
Danny-Tran <truong_tran@live.com>
2020-07-07 13:23:39 -0600

    creating Blocks component

---
+++ src/Component/home/matches/Blocks.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { firebaseMatches } from '../../../firebase';
+
+
+class Blocks extends Component {
+    
+    state = {
+        matches:[]
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Blocks;
