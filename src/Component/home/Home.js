6fbd68281197dad3f3e70327009d93f45106037f
Danny-Tran <truong_tran@live.com>
2020-08-05 16:14:46 -0600

    adding comments

--- src/Component/home/Home.js
+++ src/Component/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from './featured';
 import Matches from './matches/Matches';
 
+// Home page for feature player and matches from firebase
 const Home = () => {
     return (
         <div className='bck_blue'>
--- src/HighOrderComponent/Layout.js
+++ src/HighOrderComponent/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../Component/Header_Footer/Header'
 import Footer from '../Component/Header_Footer/Footer'
 
-
+// host header and footer
 const Layout = (props) => {
     return (
         <div>
