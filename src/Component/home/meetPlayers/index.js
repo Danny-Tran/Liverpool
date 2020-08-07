7e08547d2a8cff659eec8dc22eae850d94abab4b
Danny-Tran <truong_tran@live.com>
2020-08-07 15:50:04 -0600

    adding player card with animate

--- src/Component/home/meetPlayers/index.js
+++ src/Component/home/meetPlayers/index.js
@@ -1,68 +1,86 @@
 import React, { Component } from 'react';
 import Stripes from '../../../Resources/images/stripes.png';
-import { Tag } from '../../ui/misc'
+import { Tag } from '../../ui/misc';
+import Reveal from 'react-reveal/Reveal';
+import Cards from './cards'
 
 class MeetPlayers extends Component {
 
+    state = {
+        show:false
+    }
 
     render() {
         return (
-            <div className='home_meetplayers'
-                style={{background:`#ffffff url(${Stripes})`}}
+            <Reveal
+                fraction={0.7}
+                onReveal={()=>{
+                    this.setState({
+                        show:true
+                    })
+                }}
             >
-                <div className='container'>
-                    <div className='home_meetplayers_wrapper'>
-                        <div className='home_card_wrapper'>
-                            card
+                <div className='home_meetplayers'
+                    style={{ background: `#ffffff url(${Stripes})` }}
+                >
+                    <div className='container'>
+                        <div className='home_meetplayers_wrapper'>
+                            <div className='home_card_wrapper'>
+                                <Cards
+                                    show={this.state.show}
+                                >
+
+                                </Cards>
                         </div>
-                        <div className='home_text_wrapper'>
-                            <div>
-                                <Tag bck= '#009688' size= '100px' color= '#ffffff'
-                                    add= {{
-                                        display:'inline-block',
-                                        marginBottom: '20px',
-                                    }}
-                                >
-                                    Meet
+                            <div className='home_text_wrapper'>
+                                <div>
+                                    <Tag bck='#009688' size='100px' color='#ffffff'
+                                        add={{
+                                            display: 'inline-block',
+                                            marginBottom: '20px',
+                                        }}
+                                    >
+                                        Meet
                                 </Tag>
-                            </div>
-                            <div>
-                                <Tag bck= '#009688' size= '100px' color= '#ffffff'
-                                    add={{
-                                        display:'inline-block',
-                                        marginBottom: '20px',
-                                    }}
-                                >
-                                    The
+                                </div>
+                                <div>
+                                    <Tag bck='#009688' size='100px' color='#ffffff'
+                                        add={{
+                                            display: 'inline-block',
+                                            marginBottom: '20px',
+                                        }}
+                                    >
+                                        The
                                 </Tag>
-                            </div>
-                            <div>
-                                <Tag bck= '#009688' size= '100px' color= '#ffffff'
-                                    add= {{
-                                        display:'inline-block',
-                                        marginBottom: '20px',
-                                    }}
-                                >
-                                    Players
+                                </div>
+                                <div>
+                                    <Tag bck='#009688' size='100px' color='#ffffff'
+                                        add={{
+                                            display: 'inline-block',
+                                            marginBottom: '20px',
+                                        }}
+                                    >
+                                        Players
                                 </Tag>
-                            </div>
-                            <div>
-                                <Tag bck= '#ffffff' size= '27px' color= '#009688'
-                                    link={true}
-                                    linkto='/the_team'
-                                    add= {{
-                                        display:'inline-block',
-                                        marginBottom: '27px',
-                                        border: '1px solid #009688'
-                                    }}
-                                >
-                                    Meet Them Here
+                                </div>
+                                <div>
+                                    <Tag bck='#ffffff' size='27px' color='#009688'
+                                        link={true}
+                                        linkto='/the_team'
+                                        add={{
+                                            display: 'inline-block',
+                                            marginBottom: '27px',
+                                            border: '1px solid #000000'
+                                        }}
+                                    >
+                                        Meet Them Here
                                 </Tag>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Reveal>
         );
     }
 }
