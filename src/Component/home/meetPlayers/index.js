d1415c78515feb079cfd0308edc106772810856e
Danny-Tran <truong_tran@live.com>
2020-08-07 14:00:16 -0600

    reinstalling dependencies

---
+++ src/Component/home/meetPlayers/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Stripes from '../../../Resources/images/stripes2.png';
+
+class MeetPlayer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MeetPlayer;
