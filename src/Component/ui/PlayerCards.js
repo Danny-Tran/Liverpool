95def6ae1a702b7551dc21f3ed3c776146def8a2
Danny-Tran <truong_tran@live.com>
2020-08-10 11:11:07 -0600

    complete player cards in meet the player components

--- src/Component/ui/PlayerCards.js
+++ src/Component/ui/PlayerCards.js
@@ -6,6 +6,17 @@ const PlayerCards = (props) => {
             <div className='player_card_thmb'
                 style={{background:`#f2f9ff url(${props.bck})`}}
             ></div>
+
+            <div className= 'player_card_nfo'>
+                <div className='player_card_number'>
+                    {props.number}
+                </div>
+
+                <div className='player_card_name'>
+                    <span>{props.name}</span>
+                    <span>{props.lastname}</span>
+                </div>
+            </div>
         </div>
     );
 };
