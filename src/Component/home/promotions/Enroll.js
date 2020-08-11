0fdf128160680da808d847408b49ca9321476922
Danny-Tran <truong_tran@live.com>
2020-08-11 10:17:09 -0600

    creating enroll component for email sign up

---
+++ src/Component/home/promotions/Enroll.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade'
+class Enroll extends Component {
+
+    
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+import Reveal from 'react/'
+export default Enroll;
